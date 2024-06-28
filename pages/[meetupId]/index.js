74dccb9e5d563120684f54af6352a0368c5541a7
Deepak kumar <kumarsahudeepak1999@gmail.com>
2024-06-28 15:47:49 +0530

    3rd commit

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,25 @@
 
 import MeetupDetail from '../../components/meetups/MeetupDetail.js';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
     return(
-        <MeetupDetail image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg" title="First Meetup" address="Some Street 5, Some City" description="This is a first meetup"/>
+        <MeetupDetail image={props.image} title={props.title} address={props.address} description={props.description}/>
     )
 }
 
+export async function getStaticProps(context){
+    //fetch data from an API
+    const meetupId = context.params.meetupId;
+    return{
+        props:{
+            meetupData : {
+                image:"https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+                id: meetupId,
+                title:"First Meetup" ,
+                address:"Some Street 5, Some City",
+                description:"This is a first meetup"
+            }
+        }
+    }
+}
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,30 @@ const DUMMY_MEETUPS = [
         description: 'Thid is a second meetup!'
     },
 ]
-export default function HomePage(){
+export default function HomePage(props){
     return (
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <MeetupList meetups={props.meetups} />
     )
+}
+
+// export async function getServerSideProps(context){
+//     const req = context.req;
+//     const res = context.res;
+
+//     //fetch data from an API
+//     return {
+//         props:{
+//             meetups: DUMMY_MEETUPS
+//         }
+//     }
+// }
+
+export async function getStaticProps(){
+   //fetch data from an API
+    return {
+        props:{
+            meetups: DUMMY_MEETUPS
+        },
+        revalidate: 1
+    };
 }
